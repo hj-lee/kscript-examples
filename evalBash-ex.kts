b50c84599ddbca36aecfbdca0ab534f50af66da1
Hwijae Lee <hj.d.lee@gmail.com>
2018-01-16 18:18:08 -0500

    Improve evalBash-ex.kts

--- evalBash-ex.kts
+++ evalBash-ex.kts
@@ -1,26 +1,56 @@
 
 @file:Include("evalBash.kt")
 
+fun runBashes(vararg cmds: String) {
+    cmds.forEach {
+        println("run:$it")
+        it.runBash()
+        println()
+    }
+}
+
+fun evalBashes(vararg cmds: String) {
+    cmds.forEach {
+        println("eval:$it")
+        println(it.evalBash())
+    }
+}
+
+val cmds_old = arrayOf("ls -ld .",
+        "echo 'a\\ \\\" ' \\'  \'",
+        "ls ./../",
+        "ls /home/",
+        "echo C:\\\\. ",
+        "kscript println\\(1\\)",
+        "printf 'single quote'",
+        "printf \"double quote\"",
+        "printf \"back n \\n after newline\"",
+        "kscript 'println(\"a\\nb c d\\nlast line \")'"
+)
+
+val cmds = arrayOf("ls -ld .",
+        // "echo \"a\\ \\\" ' \\'  \"",
+        // "echo 'b c / \" \\\\ \\\" '",
+        // "echo /a/b/c/d/e/f/",
+        "echo C:\\\\. ",
+        "echo 'C:\\\\\\\\.' ",
+        "kscript println\\(1\\)",
+        // "echo 'single quote'",
+        // "echo \"double quote\"",
+        // "echo \"back n \\n after newline\"",
+        "kscript 'println(\"a\\nb c d\\nlast line \")'"
+)
+
 try {
     println("runBash")
-    "ls -ld . ".runBash()
-    "kscript println\\(1\\)".runBash()
-    "printf 'single quote'".runBash()
-    "printf \"double quote\"".runBash()
-    "printf \"back n \\n after newline\"".runBash()
-    "kscript 'println(\"a\\nb c d\\nlast line \")'".runBash()
+    runBashes(*cmds)
 } catch (e: Exception) {
     e.printStackTrace()
 }
 
 try {
     println("evalBash")
-    println("ls -ld .".evalBash())
-    println("kscript println\\(1\\)".evalBash())
-    println("printf 'single quote'".evalBash())
-    println("printf \"double quote\"".evalBash())
-    println("printf \"back n \\n after newline\"".evalBash())
-    println("kscript 'println(\"a\\nb c d\\nlast line \")'".evalBash())
+    evalBashes(*cmds)
 } catch (e: Exception) {
     e.printStackTrace()
 }
